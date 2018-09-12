0931b143064cd84bda9018bd00993798e91efdc1
CaioSonoda <ra94045@uem.br>
2018-09-12 16:52:09 -0300

    main implementada

--- touch.c
+++ touch.c
@@ -16,11 +16,22 @@ double mul(double x,double y){
 }
 
 double div (double x,double y){
-
-	return x/y;
+	if(y!=0) return x/y;
+	else return -1;
 }
 
 int main(){
-
+	int x,y,opt;
+	printf("Digite um numero x e um numero y:");
+	scanf("%d %d",&x,&y);
+	printf("Digite uma operacao: \n 1- soma\n 2- sub\n 3- mul\n 4- div\n");
+	scanf("%d",&opt);
+	
+	switch (opt){
+	case 1: printf("%lf", soma(x,y)); break;
+	case 2: printf("%lf", sub(x,y)); break;
+	case 3: printf("%lf", mul(x,y)); break;
+	case 4: printf("%lf", div(x,y)); break;
 	return 0;
+	}
 }
